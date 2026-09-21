4b24f09cf96745d34ecc824db8af8017758e9465
User <User@DESKTOP-L7RC180>
2026-09-21 18:00:56 +0530

    Changed TPP

--- VVT/src/main/java/genericUtility/TPP.java
+++ VVT/src/main/java/genericUtility/TPP.java
@@ -1,5 +1,22 @@
 package genericUtility;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class TPP {
+
+	@FindBy(linkText = "Organization")
+	private WebElement contactsLink;
+
+	@FindBy(linkText = "Campaigns")
+	private WebElement campaignsLink;
+
+
+
+	public WebElement getOrganization() {
+		// TODO Auto-generated method stub
+		return contactsLink;
+
+	}
 
 }
